cf2455c4874e4ccecc5ae5e39046bca2fb83ebc1
Marwa  Awwad <marwaawwad10@gmail.com>
2021-06-08 16:35:15 +0000

    Automatically backed up by Learn

---
+++ js/challenge.js
@@ -0,0 +1,49 @@
+let c=0,i=0;
+var intervalID = setInterval(count , 1000);
+
+
+let p=document.getElementById('plus');
+let n=document.getElementById('minus');
+let h=document.getElementById('heart');
+let po=document.getElementById('pause');
+let times=0;
+
+
+function count(){
+
+  p.addEventListener("click", function(){
+  document.getElementById("counter").innerHTML = c++;})
+
+  n.addEventListener("click", function(){
+  document.getElementById("counter").innerHTML = c--;})
+  c++;
+  document.getElementById('counter').innerHTML=c;}
+
+let likes=document.getElementsByClassName("likes");
+likes=document.getElementById('lik');
+  likes.addEventListener("click", function(){
+  document.getElementsByTagName('ul').innerText = "HI";})
+
+
+
+/*
+document.getElementsByClassName('likes').addEventListener()("click" , function (){let cc=parseInt(document.getElementById('counter').innerText);
+let dis=parseInt(document.getElementsByClassName('likes').innerText)
+document.getElementsByTagName("ul").innerHTML+=`${cc} likes ${dis} times`;});
+/*function likes(){
+  if(h.clicked==true) {
+    times++;
+    let msg=`${c} was liked ${times} times`;
+  document.getElementsByClassName('likes').innerHTML+=msg;
+
+  }}
+
+
+/*
+function pause(){
+  if(document. getElementById('pause'). clicked == true)
+{
+alert("button was clicked");
+}
+}
+*/
